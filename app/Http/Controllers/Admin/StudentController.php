a18732c0a7097854eabf26b3b7f1523fd1f442c5
JankauDausat15 <isatdausat@gmail.com>
2025-11-20 12:28:33 +0700

    dashboard

---
+++ app/Http/Controllers/Admin/StudentController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('admin.student.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.student.create');
+    }
+
+    /** 
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nis' => 'required|unique:students',
+            'nama_lengkap' => 'required',
+            'jenis_kelamin' => 'required',
+            'nisn' => 'required|unique:students',
+        ]);
+
+        Student::create($request->all());
+        return redirect()->route('admin.students.index')->with('success', 'Data berhasil disimpan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $student = Student::findOrFail($id);
+        return view('admin.student.show', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $student = Student::findOrFail($id);
+        return view('admin.student.edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = Student::findOrFail($id);
+
+        $validated = $request->validate([
+            'nis' => 'required|unique:students,nis,' . $id,
+            'nama_lengkap' => 'required',
+            'jenis_kelamin' => 'required',
+            'nisn' => 'required|unique:students,nisn,' . $id,
+        ]);
+
+        $student->update($validated);
+
+        return redirect()->route('admin.students.index')->with('success', 'Data siswa berhasil diperbarui!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $student = Student::findOrFail($id);
+        $student->delete();
+
+        return redirect()->route('admin.students.index')->with('success', 'Data siswa berhasil dihapus!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\StudentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
@@ -9,4 +10,5 @@ Route::get('/', [LandingController::class, 'index']);
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::resource('students', StudentController::class);
 });
