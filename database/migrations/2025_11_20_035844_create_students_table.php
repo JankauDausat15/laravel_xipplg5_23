f0d96a956dd79d7a592010b4668a0c39e762a4a0
JankauDausat15 <isatdausat@gmail.com>
2025-11-20 11:20:27 +0700

    add migration student

--- database/migrations/2025_11_20_035844_create_students_table.php
+++ database/migrations/2025_11_20_035844_create_students_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('nis')->unique();
-            $table->string('nama_lengkap');
-            $table->enum('jenis_kelamin', ['L' , 'P']);
-            $table->string('nisn')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
